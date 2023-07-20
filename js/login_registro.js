2f3aa854b9d90b58f10ec97081a965fd4c4004dd
Erick R <epramirez2@espe.edu.ec>
2023-07-19 22:51:14 -0500

    SweetAlert para cerrar sesión

--- js/login_registro.js
+++ js/login_registro.js
@@ -62,3 +62,24 @@ $("#register_button").click(function (e) {
         }
     });
 });
+
+// Evento de clic en el enlace "Cerrar sesión"
+$("#logoutLink").click(function(e) {
+    e.preventDefault();
+
+    // Mostrar la alerta de confirmación para cerrar sesión
+    Swal.fire({
+        icon: 'question',
+        title: '¿Deseas cerrar sesión?',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, cerrar sesión',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        // Si el usuario confirma cerrar sesión, redirige a la página de login
+        if (result.isConfirmed) {
+            window.location.href = "login.html";
+        }
+    });
+});
